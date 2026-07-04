ee43ea711807e73b0f36a24f13e972a430e35e13
HassaanMinhas <FaheemKhan540123@gmail.com>
2026-07-04 09:38:11 +0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Set global API prefix
-  app.setGlobalPrefix('apiss');
+  app.setGlobalPrefix('apis');
 
 
   const allowedOrigins = process.env.ALLOWED_ORIGINS
